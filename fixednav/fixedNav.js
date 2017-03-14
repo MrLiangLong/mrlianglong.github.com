491cfea83a470ddb2ce0bb28f144600ca01874da
iampc <63117714@qq.com>
2017-03-14 11:56:40 +0800

    1

--- fixednav/fixedNav.js
+++ fixednav/fixedNav.js
@@ -2,14 +2,14 @@
       		var arry;
         		arry=[
         			{"name":"首页","href":"https://mrlianglong.github.io/","url":"/mrlianglong.github.com/fixednav/icon/index.png"},
+        			{"name":"个人网站","href":"https://mrlianglong.github.io/PersonWeb/","url":"/mrlianglong.github.com/fixednav/icon/person.png"},
+        			{"name":"webapp-H5","href":"https://mrlianglong.github.io/SmallWebApp/","url":"/mrlianglong.github.com/fixednav/icon/webapp.png"},
+        			{"name":"MyToDo小应用","href":"https://mrlianglong.github.io/MyTodoApp/","url":"/mrlianglong.github.com/fixednav/icon/mytodo.png"},
         			{"name":"100du商城","href":"https://mrlianglong.github.io/100duShoppingStore/","url":"/mrlianglong.github.com/fixednav/icon/100du.png"},
-        			{"name":"换脸小游戏","href":"https://mrlianglong.github.io/jQueryDemo/TheGameOfChangeFace","url":"/mrlianglong.github.com/fixednav/icon/huanlian.png"},
         			{"name":"响应式-理财宝","href":"https://mrlianglong.github.io/ResponsiveWebLCB/","url":"/mrlianglong.github.com/fixednav/icon/licaibao.png"},
         			{"name":"理财通","href":"https://mrlianglong.github.io/LiCaiTong/","url":"/mrlianglong.github.com/fixednav/icon/licaitong.png"},
-        			{"name":"MyToDo小应用","href":"https://mrlianglong.github.io/MyTodoApp/","url":"/mrlianglong.github.com/fixednav/icon/mytodo.png"},
-        			{"name":"21点扑克","href":"https://mrlianglong.github.io/jQueryDemo/TheGameOf21PointPoker","url":"/mrlianglong.github.com/fixednav/icon/puke.png"},
-        			{"name":"webapp-H5","href":"https://mrlianglong.github.io/SmallWebApp/","url":"/mrlianglong.github.com/fixednav/icon/webapp.png"},
-        			{"name":"个人网站","href":"https://mrlianglong.github.io/PersonWeb/","url":"/mrlianglong.github.com/fixednav/icon/person.png"}
+        			{"name":"换脸小游戏","href":"https://mrlianglong.github.io/jQueryDemo/TheGameOfChangeFace","url":"/mrlianglong.github.com/fixednav/icon/huanlian.png"},
+        			{"name":"21点扑克","href":"https://mrlianglong.github.io/jQueryDemo/TheGameOf21PointPoker","url":"/mrlianglong.github.com/fixednav/icon/puke.png"}
         		];
         		       		
         		mkSideBar(arry);
